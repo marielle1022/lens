74f0e33db8745d8169160c33360bc44e68c99bd7
Nat Tuck <nat@ferrus.net>
2019-10-28 11:54:12 -0400

    Configure for SPA

--- lib/lens_web/router.ex
+++ lib/lens_web/router.ex
@@ -17,6 +17,7 @@ defmodule LensWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/users", PageController, :index
   end
 
   # Other scopes may use custom stacks.
