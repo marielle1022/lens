8a5d24168c86f776ccaddb07d933e7ede767864f
marielle1022 <55012214+marielle1022@users.noreply.github.com>
2019-11-20 15:29:39 -0500

    MGR -- fixed page controller

--- lib/lens_web/controllers/page_controller.ex
+++ lib/lens_web/controllers/page_controller.ex
@@ -2,7 +2,7 @@ defmodule LensWeb.PageController do
   use LensWeb, :controller
 
   def index(conn, _params) do
-    photos = Lens.Photos.recent_photos(9)
+    photos = Lens.Photos.recent_photos(0)
     render(conn, "index.html", photos: photos)
   end
 end
